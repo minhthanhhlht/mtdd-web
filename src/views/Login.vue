db480f3a7c86e74bc4890b76cd8bda331ebb22c5
minhthanhhlht <43983989+minhthanhhlht@users.noreply.github.com>
2022-12-28 22:54:14 +0700

    Update validate

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,16 +7,14 @@
           <el-col :span="16" :xs="24">
             <div class="login-box">
               <div class="login-title">Đăng nhập hệ thống</div>
-              <el-form :rules="FormRules">
+              <el-form ref="form" :model="form" :rules="rules">
                 <!-- input user -->
-                <el-form-item  prop="username">
+                <el-form-item prop="username">
                   <el-input
                     v-model="form.username"
                     placeholder="Tài khoản"
                     size="large"
                     :clearable="true"
-                    :class="!validate() ? 'error' : '' "
-                    
                   >
                     <template #prefix>
                       <i
@@ -25,7 +23,6 @@
                       ></i>
                     </template>
                   </el-input>
-                  <div v-if="!validate()" class="form-error">{{ errors.username }}</div>
                 </el-form-item>
                 <!-- input password -->
                 <el-form-item prop="password">
@@ -43,7 +40,6 @@
                       ></i>
                     </template>
                   </el-input>
-                  <div v-if="!validate()" class="form-error">{{ errors.password }}</div>
                 </el-form-item>
                 <div class="form-footer">
                   <el-checkbox>Lưu thông tin</el-checkbox>
@@ -51,7 +47,7 @@
                 <!-- Button login -->
                 <el-form-item class="form-buttons">
                   <el-button
-                    @click="onSubmit"
+                    @click="validate('form')"
                     class="login-button"
                     type="primary"
                     size="large"
@@ -76,70 +72,58 @@ import Header from "@/components/Header.vue";
 import { reactive, ref } from "vue";
 import { ElNotification } from "element-plus";
 
-import { useVuelidate } from "@vuelidate/core";
-import { required, email } from "@vuelidate/validators";
-
 export default {
   name: "Login",
   data() {
     return {
-      isActive: false,
-      v$: useVuelidate(),
-      errors: {
-        username: "",
-        password: "",
-      },
       form: {
         username: "",
         password: "",
       },
-    };
-  },
-  validations() {
-    return {
-      form: {
-        username: { required },
-        password: { required },
+      rules: {
+        username: [
+          {
+            required: true,
+            message: "Vui lòng nhập tài khoản",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: "Vui lòng nhập mật khẩu",
+            trigger: "blur",
+          },
+        ],
       },
     };
   },
-  setup() {},
+
   components: {
     Header,
   },
- 
-  methods: {  
-    validate() {
-      let isValid = true
 
-      if(this.form.username == "") {
-        this.errors.username = "Vui lòng nhập tài khoản"
-        isValid = false
-      }
-
-      if(this.form.password == "") {
-        this.errors.password = "Vui lòng nhập mật khẩu"
-        isValid = false
-      }
-
-      return isValid
-
-    }, 
-    onSubmit() {      
-      this.validate()      
-      if (!this.validate()) {
-        ElNotification({
-          title: "Hệ thống",
-          message: "Vui lòng điền đầy đủ thông tin",
-          type: "error",
-        });
-      } else {
-        ElNotification({
-          title: "Hệ thống",
-          message: "Đăng nhập thành công",
-          type: "success",
-        });
-      }
+  methods: {
+    validate(formName) {
+      this.$refs[formName].validate((valid, fields) => {        
+        if (valid) {
+          ElNotification({
+            title: "Hệ thống",
+            message: "Đăng nhập thành công",
+            type: "success",
+          });
+          this.save();
+        } else {
+          ElNotification({
+            title: "Hệ thống",
+            message: "Vui lòng nhập đầy đủ thông tin",
+            type: "error",
+          });
+        }
+      });
+    },
+    save() {
+      alert("thanh cong");
     },
   },
 };
@@ -210,7 +194,6 @@ export default {
 }
 .error {
   box-shadow: 0 0 0 1px var(--el-color-danger);
-  border-radius: var(--el-input-border-radius,var(--el-border-radius-base));
+  border-radius: var(--el-input-border-radius, var(--el-border-radius-base));
 }
-
 </style>
